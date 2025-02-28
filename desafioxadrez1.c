d37d18ba65eea9cbd2e08e8d896f3266f20682ab
sammluca <sammuel.pimentel08@gmail.com>
2025-02-28 19:06:40 -0300

    Update desafioxadrez1.c

--- desafioxadrez1.c
+++ desafioxadrez1.c
@@ -4,23 +4,25 @@ int main(){
     // Inicializando as variáveis
     
     int torre = 0;
-    int bispo = 0;
-    
+           int bispo = 0;
+                int cavalo = 1;
 
     // Movimentação da torre
 
    do
    { printf("Torre\n");
      printf("DIREITA\n");
+     printf("\n");
      torre++;
-   } while (torre < 5);
+   }  while (torre < 5);
 
    // Movimentação do bispo
 
    while (bispo < 5)
    {
-     printf("Bispo\n");
-     printf("Cima, Direita\n");
+      printf("Bispo\n");
+        printf("Cima, Direita\n");
+        printf("\n");
      
      bispo++;
    }
@@ -30,9 +32,26 @@ int main(){
     for(int rainha = 0; rainha < 8; rainha++)
     {
         printf("Rainha\n");
-        printf("Esquerda\n");
+           printf("Esquerda\n");
+           printf("\n");
+    }
 
-    }    
+    // Movimentaçao cavalo
+    
+  while (cavalo--)
+  {
+    for(int i = 0; i < 2; i++){
+    printf("Cavalo\n");
+        printf("Baixo\n");
+        printf("\n");
+    }
+    printf("Cavalo\n");
+      printf("Esquerda\n");
+      printf("\n");
+
+  }
+  
+    
 
 
 
@@ -41,4 +60,7 @@ int main(){
 
 
 return 0;
-} 
+}
+
+
+
